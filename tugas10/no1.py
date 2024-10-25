08842de3aded9a9b86c1b62d200902ce209b00e1
ardiansyah <47409108+ardian1996@users.noreply.github.com>
2024-10-25 18:00:59 +0700

    Create no1.py
    no1

---
+++ tugas10/no1.py
@@ -0,0 +1,37 @@
+# Soal 1: Hello World to Python
+print("Hello World Python")
+
+# Soal 2: Aritmatika di Python
+# Pertambahan
+print(5 + 3)
+
+# Perkurangan
+print(10 - 2)
+
+# Perkalian
+print(4 * 3)
+
+# Pembagian
+print(16 / 4)
+
+# Soal 3: Assign Variable dan Tipe Data Integer, Float
+a = 10
+b = 3
+
+# Pembagian hasil integer
+int_division = a // b
+print(int_division)
+
+# Pembagian hasil float
+float_division = a / b
+print(float_division)
+
+# Soal 4: String Operation
+firstname = "Muhammad"
+lastname = "Ardiansyah"
+print(f"Hello sanbercode, saya {firstname} {lastname}! saya siap belajar python backend development.")
+
+# Soal 5: Tipe Data
+usia = 28
+kata = 'usiaku: '
+print(kata + str(usia) + ' tahun')
